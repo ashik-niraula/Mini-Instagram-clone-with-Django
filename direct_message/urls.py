6c78d032c35da9a71c4f428a427aa60e86c703d0
Ashik Niraula <niroulaashik18@gmail.com>
2025-12-19 12:46:07 +0545

    Delete direct_message directory

--- direct_message/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from direct_message import views
-
-urlpatterns = [
-    path('',views.inbox,name='inbox'),
-
-    path('chat/<username>',views.dm,name='dm'),
-]
--- direct_message/utils.py
+++
@@ -1,1 +0,0 @@
-from post.models import Follow
