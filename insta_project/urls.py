890c906cdc4c1bb70c10dee9b91a4ec09d5383ed
ashik_niraula <niraulaashik18@gmail.com>
2025-12-21 09:15:37 +0545

    admin changes

--- insta_project/urls.py
+++ insta_project/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/ASHIK_NIROULA', admin.site.urls),
     path('', include('post.urls')),
     path('profile/', include('profilee.urls')),
     path('inbox/',include('direct_message.urls')),
